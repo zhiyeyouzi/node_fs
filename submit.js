870fc90b321dbbee72c93f7227cca8ea26487c4d
吕帅锋 <lvsf@cloudcc.com>
2022-02-06 18:21:43 +0800

    自动提交621

--- submit.js
+++ submit.js
@@ -1,8 +1,9 @@
 var child_process = require('child_process');
 
 const argv = process.argv;
-const msg = argv[2];
-
+// process.argv 请查阅  http://nodejs.cn/api/process/process_argv.html
+const msg = argv[2] || '代码更新';
+console.log("commit信息", msg);
 var cmd = `git add . && git commit -m ${msg} && git pull && git push`;
 child_process.exec(cmd, function(error, stdout, stderr) {
   console.log("error:" + error);
