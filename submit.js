5c69dec450b51ea4b62a3f3d733c23e3b0194ffe
吕帅锋 <lvsf@cloudcc.com>
2022-02-06 18:15:04 +0800

    自动提交

--- submit.js
+++ submit.js
@@ -1,27 +1,12 @@
 var child_process = require('child_process');
-// import { argv } from 'process';
 
 const argv = process.argv;
 const msg = argv[2];
 
-// var cmds = [
-//     'git add .',
-//     'git commit -m ${msg}',
-//     'git push'
-// ]
 var cmd = `git add . && git commit -m ${msg} && git pull && git push`;
 child_process.exec(cmd, function(error, stdout, stderr) {
   console.log("error:" + error);
   console.log("stdout:" + stdout);
   console.log("stderr:" + stderr);
 });
-// cmds.forEach(function (cmd, i) {
-//     setTimeout(function () {
-//         console.log(cmd);
-//         exec(cmd, function (err, stdout, stderr) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//         });
-//     }, i * 1000);
-// })
+
